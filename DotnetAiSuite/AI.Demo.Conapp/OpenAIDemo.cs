651af6f10b849b78084fa02024425c23013ed58a
Anthony Ramos <misky530@aliyun.com>
2025-01-08 23:17:56 +0800

    实现openai调用和代理调用

--- DotnetAiSuite/AI.Demo.Conapp/OpenAIDemo.cs
+++ DotnetAiSuite/AI.Demo.Conapp/OpenAIDemo.cs
@@ -1,4 +1,8 @@
-﻿namespace AI.Demo.ConApp;
+﻿using System.ClientModel;
+using System.Diagnostics;
+using OpenAI;
+
+namespace AI.Demo.ConApp;
 
 using OpenAI.Chat;
 
@@ -12,4 +16,25 @@ public static class OpenAiDemo
 
         Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
     }
+
+    public static void RunProxy()
+    {
+        var option = new OpenAIClientOptions
+        {
+            Endpoint = new Uri("https://api.openai-proxy.org/v1")
+        };
+
+        var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+        Debug.Assert(apiKey != null, nameof(apiKey) + " != null");
+        var credentials = new ApiKeyCredential(apiKey);
+
+        // 初始化 OpenAIClient
+        var client = new OpenAIClient(credentials, option);
+        var chat = client.GetChatClient(model: "gpt-3.5-turbo");
+
+        ChatCompletion completion = chat.CompleteChat("What’s the weather like in San Francisco?");
+
+        Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+    }
+    
 }
--- DotnetAiSuite/AI.Demo.Conapp/Program.cs
+++ DotnetAiSuite/AI.Demo.Conapp/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AI.Demo.ConApp;
+
 Console.WriteLine("Hello, World!");
+
+OpenAiDemo.RunProxy();
