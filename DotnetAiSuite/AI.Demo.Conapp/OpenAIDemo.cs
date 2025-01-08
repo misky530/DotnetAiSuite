b362cffcb5e4b5cb04c0c321cd935b7c1c2c9046
Anthony Ramos <misky530@aliyun.com>
2025-01-08 21:48:27 +0800

    add open-ai dependency

---
+++ DotnetAiSuite/AI.Demo.Conapp/OpenAIDemo.cs
@@ -0,0 +1,15 @@
+﻿namespace AI.Demo.ConApp;
+
+using OpenAI.Chat;
+
+public static class OpenAiDemo
+{
+    public static void Run()
+    {
+        ChatClient client = new(model: "gpt-4o", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+
+        ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
+
+        Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+    }
+}
