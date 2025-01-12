8609e32886aa663d911e5d16a4b6fe83984f74b5
Anthony Ramos <misky530@aliyun.com>
2025-01-12 19:51:12 +0800

    add Simple Chat Streaming

--- DotnetAiSuite/OpenAI.Examples/Chat/Example02_SimpleChatStreaming.cs
+++ DotnetAiSuite/OpenAI.Examples/Chat/Example02_SimpleChatStreaming.cs
@@ -12,10 +12,33 @@ public partial class ChatExamples
     {
         ChatClient client = new(model: "gpt-4o", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
 
-        CollectionResult<StreamingChatCompletionUpdate> completionUpdates = client.CompleteChatStreaming("Say 'this is a test.'");
+        CollectionResult<StreamingChatCompletionUpdate> completionUpdates =
+            client.CompleteChatStreaming("Say 'this is a test.'");
 
         Console.Write($"[ASSISTANT]: ");
         foreach (StreamingChatCompletionUpdate completionUpdate in completionUpdates)
+        {
+            if (completionUpdate.ContentUpdate.Count > 0)
+            {
+                Console.Write(completionUpdate.ContentUpdate[0].Text);
+            }
+        }
+    }
+
+    [Test]
+    public void Example02_SimpleChat_Proxy_Streaming()
+    {
+        var (option, credentials) = ProxyBuilder.Build();
+
+        // 初始化 OpenAIClient
+        var iOpenAiClient = new OpenAIClient(credentials, option);
+        var client = iOpenAiClient.GetChatClient(model: "gpt-4o");
+
+        CollectionResult<StreamingChatCompletionUpdate> completionUpdates =
+            client.CompleteChatStreaming("Say 'this is a test.'");
+
+        Console.Write($"[ASSISTANT]: ");
+        foreach (var completionUpdate in completionUpdates)
         {
             if (completionUpdate.ContentUpdate.Count > 0)
             {
--- DotnetAiSuite/OpenAI.Examples/Chat/Example02_SimpleChatStreamingAsync.cs
+++ DotnetAiSuite/OpenAI.Examples/Chat/Example02_SimpleChatStreamingAsync.cs
@@ -24,4 +24,25 @@ public partial class ChatExamples
             }
         }
     }
+    
+    [Test]
+    public async Task Example02_SimpleChat_Proxy_StreamingAsync()
+    {
+        var (option, credentials) = ProxyBuilder.Build();
+
+        // 初始化 OpenAIClient
+        var iOpenAiClient = new OpenAIClient(credentials, option);
+        var client = iOpenAiClient.GetChatClient(model: "gpt-4o");
+
+        AsyncCollectionResult<StreamingChatCompletionUpdate> completionUpdates = client.CompleteChatStreamingAsync("Say 'this is a test.'");
+
+        Console.Write($"[ASSISTANT]: ");
+        await foreach (var completionUpdate in completionUpdates)
+        {
+            if (completionUpdate.ContentUpdate.Count > 0)
+            {
+                Console.Write(completionUpdate.ContentUpdate[0].Text);
+            }
+        }
+    }
 }
